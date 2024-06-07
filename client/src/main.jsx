ec4547e3b9f36c7969a828d301398602ce55bbdf
geetika1312 <121809849+geetika1312@users.noreply.github.com>
2024-06-07 17:36:26 +0530

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,12 @@
+// src/index.js or src/index.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { CssBaseline } from '@mui/material';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <CssBaseline />
+    <App />
+  </>
+);
